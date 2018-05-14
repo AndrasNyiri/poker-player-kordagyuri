a26a2421f9797f0a4ff154867e44e1ee4e30235d
Adel Kovacs <kovacsadel12@gmail.com>
2018-05-14 16:35:47 +0200

    fds

--- player.py
+++ player.py
@@ -33,7 +33,8 @@ class Player:
 
             ranking = deck.get_ranking()
             max_rank = 60 * 30.0
-            print card_list
+            for card in card_list:
+                print card.get_suit() + " " + card.get_value()
             print ranking / max_rank
             return max(int(ranking / max_rank * stack), game_state["minimum_raise"])
 
